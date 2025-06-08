bc8bbc2209609ddbc611470458140717e3515300
ihor-n <your-email@example.com>
2025-06-08 16:25:41 +0200

    removed next/font/google

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -1,9 +1,6 @@
-import { Inter } from 'next/font/google'
 import { Navigation } from '@/components'
 
 import '@/app/globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'User Management Dashboard'
@@ -15,7 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <link rel="icon" href="/favicon.ico" />
       </head>
-      <body className={inter.className}>
+      <body>
         <Navigation />
         <main>{children}</main>
       </body>
